5e50ad54638da355ad9b89c78659bbe1562bf570
aanatema <romane.boireau@gmail.com>
2023-11-29 10:49:29 +0100

    fonction swapForbbidenWords fonctionnelle avec un seul élément de comparaison, WIP: tableau d'éléments

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 //je veux échanger la valeur de "input" par la valeur de "transform"
 
-function getValue(){
-    let input = document.getElementById("input").value
-    let transform = "🌟💌🌸💖"
-    input = transform
-    console.log(input)
-}
-getValue();
+let transform = "🌟💌🌸💖";
 
+// function getValue() {
+//   let input = document.getElementById("input").value;
+//   input = transform;
+//   console.log(input);
+// }
+// getValue();
+
+//je veux créer un tableau qui contiendra une suite de mots, 
+//ces mots seront comparés avec la valeur de l'input 
+//la valeur de l'input sera changée si il y a correspondance
+
+let forbiddenWords = "oli" 
+//["azerty", "caramel", "santa", "téléportation"];
+
+function swapForbiddenWords() {
+    let input = document.getElementById("input").value;
+    if(input == forbiddenWords){
+        input = transform
+        console.log(input)
+
+    }else{
+        console.log(input)
+    }
+    return input
+    
+}
+
+swapForbiddenWords();
