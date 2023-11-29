bc8132a0ea612483c57a5db3bf187e69915c92e4
aanatema <romane.boireau@gmail.com>
2023-11-29 10:24:45 +0100

    creation de la fonction getValue() qui récupère la valeur de l'input et l'échange contre une suite d'émojis

---
+++ script.js
@@ -0,0 +1,10 @@
+//je veux échanger la valeur de "input" par la valeur de "transform"
+
+function getValue(){
+    let input = document.getElementById("input").value
+    let transform = "🌟💌🌸💖"
+    input = transform
+    console.log(input)
+}
+getValue();
+
