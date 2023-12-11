cfbbd0e99a56d88f708ddcaad56f28caa90ad651
aanatema <romane.boireau@gmail.com>
2023-12-11 09:21:50 +0100

    changements pour essayer de refaire fonctionner l'ajout de mots dans le tableau des mots interdits

--- script.js
+++ script.js
@@ -15,16 +15,16 @@ let forbiddenWords = ["la", "de", "un"];
 
 //déclenchement des fonctions
 //déclenche la fonction saveForbiddenWord lorsque l'on clique sur le bouton "SaveButton"
-document.getElementById("save_button").addEventListener("click", saveForbiddenWord);
 let htmlInput = document.getElementById("html_input").value;
+document.getElementById("save_button").addEventListener("click", saveForbiddenWord);
 
-document.getElementById("button").addEventListener("click", swapForbiddenWords);
-document.getElementById("show_button").addEventListener("click", showForbiddenWords);
+
+// document.getElementById("button").addEventListener("click", swapForbiddenWords);
+// document.getElementById("show_button").addEventListener("click", showForbiddenWords);
 
 //permet d'ajouter des mots à bannir
 function saveForbiddenWord() {
-  //remplacer à terme if par un switch
-  if (htmlInput != "" && htmlInput != " " && htmlInput != forbiddenWords) {
+  if (htmlInput != "" && htmlInput != " " && htmlInput != forbiddenWords ) {
     forbiddenWords.push(htmlInput);
     console.log("tableau fbw:", forbiddenWords);
   } else {
@@ -33,43 +33,43 @@ function saveForbiddenWord() {
 saveForbiddenWord();
 
 // permet d'iterer sur le contenu de la page et de voir si il y a des correspondances avec le tableau "forbiddenWords"
-function scanAndReplace() {
-  const text = document.querySelectorAll(
-    "h1",
-    "h",
-    "h3",
-    "h4",
-    "h5",
-    "h6",
-    "p"
-  );
+// function scanAndReplace() {
+//   const text = document.querySelectorAll(
+//     "h1",
+//     "h",
+//     "h3",
+//     "h4",
+//     "h5",
+//     "h6",
+//     "p"
+//   );
 
-  for (let i = 0; i < text.length; i++) {
-    if (text[i].innerHTML.includes(forbiddenWords)) {
-      text[i].innerHTML = text[i].innerHTML.replace(transform);   
-    } else {
-    }
-  }
-}
-scanAndReplace();
+//   for (let i = 0; i < text.length; i++) {
+//     if (text[i].innerHTML.includes(forbiddenWords)) {
+//       text[i].innerHTML = text[i].innerHTML.replace(transform);   
+//     } else {
+//     }
+//   }
+// }
+// scanAndReplace();
 
-function swapForbiddenWords() {
-  //boucle for pour itérer sur le tableau et comparer chaque élément avec la valeur de l'input
-  for (let i = 0; i < forbiddenWords.length; i++) {
-    if (htmlInput == forbiddenWords[i]) {
-      htmlInput = transform;
-      console.log("show swaped html input:", htmlInput);
-      break;
-    } else {
-      console.log("show non swaped html input:", htmlInput);
-    }
-  }
-}
-swapForbiddenWords();
+// function swapForbiddenWords() {
+//   //boucle for pour itérer sur le tableau et comparer chaque élément avec la valeur de l'input
+//   for (let i = 0; i < forbiddenWords.length; i++) {
+//     if (htmlInput == forbiddenWords[i]) {
+//       htmlInput = transform;
+//       console.log("show swaped html input:", htmlInput);
+      
+//     } else {
+//       console.log("show non swaped html input:", htmlInput);
+//     }
+//   }
+// }
+// swapForbiddenWords();
 
-function showForbiddenWords() {
-  document.getElementById("forbidden_list").innerHTML = forbiddenWords;
-}
+// function showForbiddenWords() {
+//   document.getElementById("forbidden_list").innerHTML = forbiddenWords;
+// }
 
-//affiche la suite d'emojis actuellement séléctionnée
-document.getElementById("emoji_suite").innerHTML = transform;
+// //affiche la suite d'emojis actuellement séléctionnée
+// document.getElementById("emoji_suite").innerHTML = transform;
